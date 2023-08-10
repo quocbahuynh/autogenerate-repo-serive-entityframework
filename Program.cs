622cb7c8358cdc23ba04d754ad33b423c53136be
quocbahuynh <jbrteamwebsite@gmail.com>
2023-08-10 11:35:50 +0700

    add implementation

--- Program.cs
+++ Program.cs
@@ -7,6 +7,7 @@ namespace RepositoryGenerator
     {
         static void Main(string[] args)
         {
+            var dbContext = @"ClubMembershipContext";
             var modelsPath = @"C:\Users\Acer\Desktop\Projects\ASP.NET\ClubMemebership\BusinessObject\Models"; // TODO: Update with your Models directory path
             var repoInterfacePath = @"C:\Users\Acer\Desktop\Projects\ASP.NET\ClubMemebership\BusinessObject\IRepositories"; // TODO: Update where you want the interfaces
             var repoImplementationPath = @"C:\Users\Acer\Desktop\Projects\ASP.NET\ClubMemebership\BusinessObject\Repositories"; // TODO: Update where you want the implementations
@@ -15,7 +16,7 @@ namespace RepositoryGenerator
             {
                 var modelName = Path.GetFileNameWithoutExtension(file);
                 GenerateRepositoryInterface(modelName, repoInterfacePath);
-                GenerateRepositoryImplementation(modelName, repoImplementationPath);
+                GenerateRepositoryImplementation(modelName, repoImplementationPath, dbContext);
             }
         }
 
@@ -39,19 +40,49 @@ using ClubMemebership_QuocHB_WebRazorPage.Repo.Models;
             File.WriteAllText(Path.Combine(outputPath, $"I{modelName}Repository.cs"), content);
         }
 
-        private static void GenerateRepositoryImplementation(string modelName, string outputPath)
+        private static void GenerateRepositoryImplementation(string modelName, string outputPath, string dbContext)
         {
             var content = $@"using System;
 using System.Collections.Generic;
 using System.Linq;
 using ClubMemebership_QuocHB_WebRazorPage.Repo.Models;
-using  ClubMemebership_QuocHB_WebRazorPage.Repo.IRepositories
+using  ClubMemebership_QuocHB_WebRazorPage.Repo.IRepositories;
 
 namespace ClubMemebership_QuocHB_WebRazorPage.Repo.Repositories
 {{
     public class {modelName}Repository : I{modelName}Repository
     {{
+    private {dbContext} context;
+ 
+            public {modelName}Repository({dbContext} context)
+            {{
+                this.context = context;
+            }}
         // TODO: Implement your DbContext and actual logic here
+public void Add({modelName} entity)
+        {{
+            throw new NotImplementedException();
+        }}
+
+        public void Delete(int id)
+        {{
+            throw new NotImplementedException();
+        }}
+
+        public IEnumerable<{modelName}> GetAll()
+        {{
+            throw new NotImplementedException();
+        }}
+
+        public {modelName} GetById(int id)
+        {{
+            throw new NotImplementedException();
+        }}
+
+        public void Update({modelName} entity)
+        {{
+            throw new NotImplementedException();
+        }}
     }}
 }}";
 
